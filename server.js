2c89d03efa5a259dc69b30104b985df2d32448fd
Kamal Panwar <you@example.com>
2023-09-25 19:25:15 +0530

    'port changed'

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 
-const port = process.env.port;
+
 const DB = process.env.DATABASE.replace(
   '<password>',
   process.env.DATABASE_PASSWORD
@@ -12,6 +12,6 @@ mongoose.connect(DB).then((con) => {
   console.log('DB connections successful!');
 });
 
-app.listen(port, () => {
-  console.log(`server is running on port:${port}`);
+app.listen(3000, () => {
+  console.log(`server is running on port:3000`);
 });
